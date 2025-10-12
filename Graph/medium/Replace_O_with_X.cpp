412c4e696abd06c58e1b8d801aa68aea979e2a07
Rohit_Ranjan <ran.23031ro.hit@gmail.com>
2025-10-12 11:03:30 +0530

    Add Replace O's with X's code

---
+++ Graph/medium/Replace_O_with_X.cpp
@@ -0,0 +1,72 @@
+class Solution {
+  public:
+    int r;
+    int c;
+    int row[4] = {1,-1,0,0};
+    int col[4] = {0,0,-1,1};
+    bool valid(int i, int j){
+        return i>=0&& i<r&& j>=0&& j<c;
+    }
+    vector<vector<char>> fill(vector<vector<char>>& mat) {
+        // code here
+        r = mat.size();
+        c = mat[0].size();
+        
+        queue<pair<int,int>>q;
+        
+        // first row
+        for(int i=0; i<c; i++){
+            if(mat[0][i] == 'O'){
+                q.push(make_pair(0,i));
+                mat[0][i] = 'T';
+            }
+        }
+        // first col
+        for(int i=1; i<r; i++){
+            if(mat[i][0] == 'O'){
+                q.push(make_pair(i,0));
+                mat[i][0] = 'T';
+            }
+        }
+        // last row
+        for(int i=1; i<c; i++){
+            if(mat[r-1][i] == 'O'){
+                q.push(make_pair(r-1,i));
+                mat[r-1][i] = 'T';
+            }
+        }
+        // last col
+        for(int i=1; i<r-1; i++){
+            if(mat[i][c-1] == 'O'){
+                q.push(make_pair(i,c-1));
+                mat[i][c-1] = 'T';
+            }
+        }
+        while(!q.empty()){
+            int i = q.front().first;
+            int j = q.front().second;
+            q.pop();
+            for(int k=0; k<4; k++){
+                if(valid(i+row[k],j+col[k])&&mat[i+row[k]][j+col[k]] == 'O'){
+                    mat[i+row[k]][j+col[k]] = 'T';
+                    q.push(make_pair(i+row[k],j+col[k]));
+                }
+            }
+        }
+        
+        for(int i=0; i<r; i++){
+            for(int j=0; j<c; j++){
+                if(mat[i][j] == 'O'){
+                    mat[i][j] = 'X';
+                }
+                if(mat[i][j] == 'T'){
+                    mat[i][j] = 'O';
+                }
+                
+            }
+        }
+        
+        return mat;
+        
+    }
+};
